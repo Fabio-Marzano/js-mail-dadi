954fdfa5001cb68188bd42377de35288f82489b9
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-01 22:18:44 +0200

    [UPD] create-input-for-lose-or-win

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,3 +3,11 @@ let user_number = Math.floor(Math.random() * 6 + 1);
 
 //CREO NUMERO CASUALE PER IL PC//
 let pc_number = Math.floor(Math.random() * 6 + 1);
+
+//METTO A CONFRONTO I DUE NUMERI//
+if (user_number > pc_number) {
+    console.log("Hai vinto")
+}
+else if(user_number < pc_number) {
+    console.log("Hai perso")
+}
